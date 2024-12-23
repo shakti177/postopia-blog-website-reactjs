c64b773e454df5ec6b16a843359a7768cf693c0d
Kritik hedau <kritikhedau@gmail.com>
2024-12-23 22:33:12 +0530

    expo router setup done

---
+++ blogNest_App/app/_layout.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const _layout = () => {
+  const screenOptions = {
+    headerShown: false,
+  };
+
+  return (
+    <Stack screenOptions={screenOptions} initialRouteName='Login'>
+      <Stack.Screen name='index' />
+      <Stack.Screen name='Login' />
+    </Stack>
+  )
+}
+
+export default _layout
