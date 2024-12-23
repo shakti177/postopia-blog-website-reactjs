9039b93ed9e33a301f95591981993184e050820c
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2024-12-23 14:38:28 +0530

    refactor: Improve code formatting and structure in postController and post-router

--- backend/routes/post-router.js
+++ backend/routes/post-router.js
@@ -1,15 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {isLoggedIn} = require('../middlewares/isLoggedIn');
+const { isLoggedIn } = require("../middlewares/isLoggedIn");
 
-const { createPost, updatePost, deletePost } = require('../controllers/postController');
+const {
+  createPost,
+  updatePost,
+  deletePost,
+} = require("../controllers/postController");
 
+router.post("/create", isLoggedIn, createPost);
 
-router.post('/create', isLoggedIn, createPost);
-
-router.post("/update/:id", isLoggedIn, updatePost);
+router.patch("/update/:id", isLoggedIn, updatePost);
 
 router.post("/delete/:id", isLoggedIn, deletePost);
-
 
 module.exports = router;
