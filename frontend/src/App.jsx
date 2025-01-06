590b9de6e8374a9702d3700650ba329c9e8612e3
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2025-01-06 19:11:38 +0530

    Add NotFound page and update routing to handle 404 errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Header/Navbar";
 import Home from "./pages/Home";
 import Footerbar from "./components/Footer/Footerbar";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footerbar />
       </BrowserRouter>
