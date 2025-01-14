4bd152b212cd5d4398543be17e2bc2f0c38f972e
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2025-01-14 17:42:35 +0530

    Remove redundant class from NotFound component for cleaner markup

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <>
-      <div className="flex flex-col items-center gap-7 justify-center h-dvh bg-gray-100 dark:bg-black p-5">
+      <div className="flex flex-col items-center gap-7 justify-center h-dvh dark:bg-black p-5">
         <h1 className="text-9xl font-bold">404</h1>
         <h3 className="text-3xl font-bold">Page Not Found</h3>
         <p className="text-lg text-center md:w-1/2">
