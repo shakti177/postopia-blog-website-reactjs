399df906a10be8ed8d714362404c7382485b6def
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2025-03-02 00:35:57 +0530

    feat: Set default thumbnail URL in post model for improved consistency

--- backend/models/post-model.js
+++ backend/models/post-model.js
@@ -10,7 +10,11 @@ const postSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
-    thumbnail: String,
+    thumbnail: {
+      type: String,
+      default:
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEig2v1RtW7VyxFL-72o04C7RGJ8TrpXg_Tw1qNjoOHi30gyvEaz0PPFQ7DTzM2J9yCljMY9Y0exWi0N4oOMu_GJvQ_r6l6X3DwqqAerNBMbf6MQE6afta2q0sNZJUrKM4KVSh8j_YJNROmArCG3krxNLImhC_sg8rK1NE0kSlAGThtTTyjbm2ic_MyZLfAS/s16000/thumbnail.png",
+    },
   },
   { timestamps: true }
 );
