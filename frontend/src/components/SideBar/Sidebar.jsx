7806446e8532bc829ff2548477ba230a5a18feee
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2025-04-11 19:23:26 +0530

    fix: Add missing icons for categories in Sidebar component

--- frontend/src/components/SideBar/Sidebar.jsx
+++ frontend/src/components/SideBar/Sidebar.jsx
@@ -1,31 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  Shirt,
+  Laptop,
+  Pizza,
+  BrainCircuit,
+  TicketsPlane,
+  Code,
+} from "lucide-react";
 
 const Sidebar = () => {
   const categories = [
     {
       id: 1,
       name: "Fashion",
+      icon: <Shirt />,
     },
     {
       id: 2,
       name: "Technology",
+      icon: <Laptop />,
     },
     {
       id: 3,
       name: "Food",
+      icon: <Pizza />,
     },
     {
       id: 4,
       name: "AI",
+      icon: <BrainCircuit />,
     },
     {
       id: 5,
       name: "Travel",
+      icon: <TicketsPlane />,
     },
     {
       id: 6,
       name: "Coding",
+      icon: <Code />,
     },
   ];
 
@@ -38,6 +52,7 @@ const Sidebar = () => {
             <Link to={`/category/${data.name}`}>
               <h3 className="text-lg font-semibold bg-gray-100 dark:bg-neutral-900 rounded-xl px-4 py-4 mb-3 shadow-sm">
                 {data.name}
+                <span className="float-right">{data.icon}</span>
               </h3>
             </Link>
           </div>
