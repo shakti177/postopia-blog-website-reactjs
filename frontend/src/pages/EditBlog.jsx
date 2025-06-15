a8e4f8d6a639db4b42626505a285e7b908cd9713
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2025-06-15 11:26:30 +0530

    fix: enhance error handling with custom 404 page in EditBlog component

--- frontend/src/pages/EditBlog.jsx
+++ frontend/src/pages/EditBlog.jsx
@@ -97,9 +97,22 @@ const EditBlog = () => {
     );
   }
 
-  if (!post || post._id !== id) {
-    return <Navigate to="*" />;
-  }
+  if ((!loading && !post) || !post.author)
+    return (
+      <div className="flex flex-col items-center gap-7 justify-center h-dvh dark:bg-black p-5">
+        <h1 className="text-9xl font-bold">404</h1>
+        <h3 className="text-3xl font-bold">Page Not Found</h3>
+        <p className="text-lg text-center md:w-1/2">
+          We are very sorry for the inconvinience. Its look like you're trying
+          to acces a page that has been deleted or never even existed.
+        </p>
+        <Link to="/">
+          <Button className="bg-blue-800 hover:bg-blue-700">
+            Back to Home
+          </Button>
+        </Link>
+      </div>
+    );
 
   return (
     <div className="container mx-auto px-5 md:px-10 py-7">
